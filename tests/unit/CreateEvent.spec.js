638265bc690601c4fb06777c378e2f18862ef89e
Felix Wippich <felix.wippich@me.com>
2020-05-08 11:51:14 +0200

    Create input form scaffold for Create Event Page

--- tests/unit/CreateEvent.spec.js
+++ tests/unit/CreateEvent.spec.js
@@ -12,4 +12,28 @@ describe("CreateEvent", () => {
     const wrapper = mount(CreateEvent);
     expect(wrapper.get("h1").text()).toBe("Create an Event");
   });
+
+  // it has a form element
+  test("it has an <form> element", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.contains("form")).toBe(true);
+  });
+
+  // it should contain a title input field
+  test("it should contain a title input field", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.contains("input[type='text'][name='title']")).toBe(true);
+  });
+
+  // it should have input with submit button
+  test("it should have a clickable submit button", () => {
+    const wrapper = mount(CreateEvent);
+    expect(wrapper.contains("input[type='submit'][value='Create']")).toBe(true);
+  });
+  // it should contain an input field for the title with the placeholder 'add a title'
+  test("it should contain an input field for the title with the placeholder 'Add a Title'", () => {
+    const wrapper = mount(CreateEvent);
+    const titleInput = wrapper.get("input[name='title']");
+    expect(titleInput.attributes("placeholder")).toBe("Add a Title");
+  });
 });
