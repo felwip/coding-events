38252ed17196b0f8a75163c4c3682336e402a337
Felix Wippich <felix.wippich@me.com>
2020-05-13 10:33:51 +0200

    Add EventCard and its rendering in Dashboard

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    <h2>Event</h2>
+    <h2>{{ event.title }}</h2>
+    <h3>{{ date }}</h3>
+    <p>{{ event.details }}</p>
+    <p>{{ event.location }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["event"],
+  computed: {
+    date() {
+      return new Date(this.event.date).toDateString();
+    },
+  },
+};
 </script>
 
 <style scoped></style>
