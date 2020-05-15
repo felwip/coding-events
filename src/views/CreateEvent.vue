10a378bfedfe6111dd52b85d96d5f6dd065419dd
Felix Wippich <felix.wippich@me.com>
2020-05-15 16:11:46 +0200

    Additional input fields for create event form, TODO: testing

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -8,6 +8,19 @@
         name="title"
         placeholder="Add a Title"
       />
+      <textarea
+        v-model="event.details"
+        type="text"
+        name="details"
+        placeholder="Add event details"
+      />
+      <input v-model="event.date" type="date" name="date" />
+      <input
+        v-model="event.location"
+        type="text"
+        name="location"
+        placeholder="Add a location"
+      />
       <input type="submit" value="Create" />
     </form>
   </div>
@@ -20,21 +33,35 @@ export default {
   data() {
     return {
       event: {
-        title: ""
-      }
+        title: "",
+        details: "",
+        location: "",
+        date: "",
+      },
     };
   },
   methods: {
     async submit() {
       try {
-        const res = await createEvent(this.event);
-        console.log("Created new event", res.data);
+        await createEvent(this.event);
+        this.$router.push({ name: "dashboard" });
       } catch (error) {
-        console.error(error);
+        alert("Couldn't create entry, please check your inputs");
       }
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+form {
+  display: grid;
+  gap: 2em;
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+input {
+  display: block;
+}
+</style>
